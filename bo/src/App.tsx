f8af40e8f00ee30c2c84767c1b3af0b739e0faf2
Rodevp <rodriguezrodrigor12@gmail.com>
2023-08-07 12:12:22 -0500

    creando button component

--- bo/src/App.tsx
+++ bo/src/App.tsx
@@ -1,8 +1,14 @@
+import Button from "./components/button/Button"
+
 function App() {
 
   return (
     <>
       <div>
+        <Button
+          text="Test"
+          action={() => {}}
+        />
       </div>
     </>
   )
